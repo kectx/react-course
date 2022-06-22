673d4a1f150403dd0bccadc4a653117caf65f6d6
xtcek <mateusz.greda@tutore.eu>
2022-06-22 16:09:00 +0200

    Add new form, hiding and creating cards, few refactor things

--- react-app/src/components/Expenses/ExpenseItem.js
+++ react-app/src/components/Expenses/ExpenseItem.js
@@ -7,18 +7,20 @@ import "./ExpenseItem.css"
 const ExpenseItem = props => {
 	const [title, setTitle] = useState(props.title)
 
-	const clickHandler = () => {
-		setTitle("Updated!")
-	}
+	// const clickHandler = () => {
+	// 	setTitle("Updated!")
+	// }
 	return (
-		<Card className='expense-item'>
-			<ExpenseDate date={props.date} />
-			<div className='expense-item__description'>
-				<h2>{props.title}</h2>
-				<div className='expense-item__price'>${props.amount}</div>
-			</div>
-			<button onClick={clickHandler}>Change Title</button>
-		</Card>
+		<li>
+			<Card className='expense-item'>
+				<ExpenseDate date={props.date} />
+				<div className='expense-item__description'>
+					<h2>{props.title}</h2>
+					<div className='expense-item__price'>${props.amount}</div>
+				</div>
+				{/* <button onClick={clickHandler}>Change Title</button> */}
+			</Card>
+		</li>
 	)
 }
 
